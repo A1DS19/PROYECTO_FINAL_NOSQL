12e3d2a4d2aec581edee5fe9f3be5fe8b838c44a
A1DS19 <jp06171@gmail.com>
2020-11-27 19:26:32 -0600

    validacion extra

--- routes/products/createProduct.js
+++ routes/products/createProduct.js
@@ -18,18 +18,23 @@ router.post(
       .withMessage('Debe agregarle el precio para el producto')
       .isNumeric()
       .withMessage('El precio debe ser un numero obvio'),
+    body('countInStock')
+      .not()
+      .isEmpty()
+      .withMessage('Debe incluir el stock del producto'),
   ],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
       return res.status(401).json({ errors: errors.array() });
     }
-    const { imagePath, title, description, price } = req.body;
+    const { imagePath, title, description, price, countInStock } = req.body;
     const product = new Product({
       imagePath,
       title,
       description,
       price,
+      countInStock,
     });
     await product.save();
     res.status(201).json({ msg: 'Producto creado' });
